4125b2b384b3ea60dbf5a8e2d71ebffe093885b6
Khandaker Sajjat <me@kns.dev>
2022-03-11 14:57:32 +0600

    addec steps

--- script.js
+++ script.js
@@ -4,7 +4,27 @@ const sleep = async (ms) => {
     })
 }
 
+const addStep = (i, step) => {
+    const boxs = document.querySelectorAll('.box');
+    const child = document.createElement('div');
+    child.classList.add('steps-boxs');
+    let string = `<div data-step="${Math.abs(step) < 10 ? '0'+Math.abs(step) : Math.abs(step)}" class="step">Step</div>`;
+    let boxsNew = document.querySelectorAll('.box');
+    boxsNew.forEach((v, index) => string += `
+        <div class="steps-box" ${([i, i+1].includes(index) ? `style="border-bottom: 2px solid red"` : ``)}>${v.innerHTML}</div>
+    `);
+    child.innerHTML = string;
+    document.querySelector(".steps").prepend(child);
+}
+
+let busy = false;
 const main = async () => {
+    if(busy) return;
+    let step = 0;
+    document.querySelector('.boxs').innerText = '';
+    document.querySelector(".steps").innerText = '';
+    const condition = document.querySelector("input[name='type']:checked").value === 'asc' ? true : false;
+    busy = !busy;
     const inputBox = document.querySelector('input[name="inputBox"]');
     const nums = inputBox.value.replace(/ /g, '').split(',');
     console.log(nums);
@@ -27,20 +47,20 @@ const main = async () => {
         document.querySelector('.boxs').appendChild(elem);
     });
 
-    let needToCheck = lastIndex = numsLength;
-    while(0 < needToCheck){
+    let lastIndex = numsLength;
+    while(0 < lastIndex){
         let done = 1;
-        let boxs = document.querySelectorAll('.box');
-        let boxsLength = boxs.length;
+        const boxs = document.querySelectorAll('.box');
         for(let i=0; i<lastIndex-1; i++){
             const checkingIndex = boxs[i];
             const compareIndex = boxs[i+1];
             const checkingNum = parseFloat(checkingIndex.innerHTML);
             const compareNum = parseFloat(compareIndex.innerHTML);
             checkingIndex.setAttribute('data-content', '⬆️');
-            checkingIndex.classList.add('bgBlue', 'test');
-            compareIndex.classList.add('bgIndex');
-            if(checkingNum > compareNum){
+            checkingIndex.classList.add('bgBlue');
+            compareIndex.classList.add('bgGreen');
+            const conditionX = condition ? checkingNum > compareNum : compareNum > checkingNum;
+            if(conditionX){
                 done = 0;
                 await sleep(150);
                 checkingIndex.style.color = "red";
@@ -48,22 +68,24 @@ const main = async () => {
                 await sleep(200);
                 checkingIndex.innerHTML = compareNum;
                 compareIndex.innerHTML = checkingNum;
+                step--;
+                addStep(i, step);
             }else{
                 await sleep(350);
             }
             await sleep(500);
             checkingIndex.removeAttribute('data-content');
             checkingIndex.classList.remove('bgBlue', 'bgRed');
-            compareIndex.classList.remove('bgIndex', 'bgRed');
+            compareIndex.classList.remove('bgGreen', 'bgRed');
             checkingIndex.style.color = "black";
             compareIndex.style.color = "black";
         }
-        boxs[lastIndex-1].classList.add('bgIndex');
+        boxs[lastIndex-1].classList.add('bgGreen');
         if(done){
-            boxs.forEach((box) => box.classList.add('bgIndex'));
+            boxs.forEach((box) => box.classList.add('bgGreen'));
+            busy = !busy;
             break;
         }
-        needToCheck--;
         lastIndex--;
     }
 }
